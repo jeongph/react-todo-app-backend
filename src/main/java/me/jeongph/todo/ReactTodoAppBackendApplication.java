65e74c00a8b5973b75ac771d78340af314b2f2b8
JeongUk Park <dnr1105@gmail.com>
2025-03-10 22:52:49 +0900

    Clean sensitive information and Init

---
+++ src/main/java/me/jeongph/todo/ReactTodoAppBackendApplication.java
@@ -0,0 +1,13 @@
+package me.jeongph.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactTodoAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactTodoAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/jeongph/todo/ReactTodoAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jeongph.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactTodoAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
